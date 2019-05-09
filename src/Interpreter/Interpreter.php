3935da83da6f0675ad72e250e4e74a8702da480a
VolCh <volch5@gmail.com>
2019-05-09 23:05:07 +0300

    Interpret Plus binary operator

--- src/Interpreter/Interpreter.php
+++ src/Interpreter/Interpreter.php
@@ -2,8 +2,10 @@
 
 namespace App\Interpreter;
 
+use App\Parser\Node\BinaryOperator;
 use App\Parser\Node\IntegerLiteral;
 use App\Parser\Node\Node;
+use DomainException;
 
 /**
  * Class Interpreter
@@ -20,11 +22,20 @@ class Interpreter
     {
         $visitMethodName = "visit{$node->type()}";
         return $this->$visitMethodName($node);
-
     }
 
     private function visitIntegerLiteral(IntegerLiteral $node)
     {
         return $node->value();
+    }
+
+    private function visitBinaryOperator(BinaryOperator $node)
+    {
+        $leftValue = $this->visit($node->left());
+        $rightValue = $this->visit($node->right());
+        if ($node->operator()->value() === '+') {
+            return $leftValue + $rightValue;
+        }
+        throw new DomainException("Unknown operator {$node->operator()->value()}");
     }
 }
--- test/Interpreter/InterpreterTest.php
+++ test/Interpreter/InterpreterTest.php
@@ -3,6 +3,7 @@
 namespace App\Test\Interpreter;
 
 use App\Interpreter\Interpreter;
+use App\Parser\Node\BinaryOperator;
 use App\Parser\Node\IntegerLiteral;
 use App\Test\Test;
 use App\Tokenizer\Token;
@@ -20,5 +21,19 @@ class InterpreterTest extends Test
         $interpreter->interpret($tree);
         $content = ob_get_clean();
         $this->assertEquals($content, '1');
+    }
+
+    public function testPlus(): void
+    {
+        $interpreter = new Interpreter();
+        $tree = new BinaryOperator(
+            new IntegerLiteral(new Token(Token::TYPE_INTEGER, 1)),
+            new Token(Token::TYPE_PLUS, '+'),
+            new IntegerLiteral(new Token(Token::TYPE_INTEGER, 2))
+        );
+        ob_start();
+        $interpreter->interpret($tree);
+        $content = ob_get_clean();
+        $this->assertEquals($content, '3');
     }
 }
