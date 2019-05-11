fa6adf44bdf864d4cccf939eed74bf811f688985
VolCh <volch5@gmail.com>
2019-05-11 23:09:40 +0300

    Use reflection for token types

--- src/Tokenizer/Token.php
+++ src/Tokenizer/Token.php
@@ -3,6 +3,7 @@
 namespace App\Tokenizer;
 
 use InvalidArgumentException;
+use ReflectionClass;
 
 /**
  * Class Token
@@ -15,17 +16,15 @@ class Token
     public const TYPE_PLUS = 'PLUS';
     public const TYPE_MINUS = 'MINUS';
 
-    private const TYPES = [self::TYPE_INTEGER, self::TYPE_PLUS, self::TYPE_MINUS, self::TYPE_MULTI, self::TYPE_DIV];
-
     /** @var string */
     private $type;
     /** @var mixed */
     private $value;
 
-    public function __construct(string $type, $value )
+    public function __construct(string $type, $value)
     {
-        if (!in_array($type, self::TYPES, true)) {
-            throw new InvalidArgumentException("Not supported type $type");
+        if (!in_array($type, self::types(), true)) {
+            throw new InvalidArgumentException("Unknown type '$type'");
         }
         $this->type = $type;
         $this->value = $value;
@@ -40,5 +39,17 @@ class Token
     public function value()
     {
         return $this->value;
+    }
+
+    /** @return string[] */
+    private static function types(): array
+    {
+        static $types;
+
+        if (empty($types)) {
+            $types = (new ReflectionClass(self::class))->getConstants();
+        }
+
+        return $types;
     }
 }
